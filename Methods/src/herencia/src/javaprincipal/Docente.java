a1991cbc0aaddbaea26707518c2752da2949b9f3
Joshua Inciarte <153563236+Inciart@users.noreply.github.com>
2025-10-24 06:42:36 -0600

    Add files via upload

---
+++ Methods/src/herencia/src/javaprincipal/Docente.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaprincipal;
+
+/**
+ *
+ * @author User
+ */
+public class Docente {
+    private String nombres;
+    private String apellidos;
+    private String documento;
+    private String codigo;
+
+    public Docente(String nombres, String apellidos, String documento, String codigo) {
+        this.setNombres(nombres);
+        this.setApellidos(apellidos);
+        this.setCodigo(codigo);
+        this.setCodigo(codigo);
+    }
+    
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+}
---
+++ Methods/src/herencia/src/javaprincipal/Persona.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaprincipal;
+
+/**
+ *
+ * @author User
+ */
+public class Persona {
+    private String nombres;
+    private String apellidos;
+    private String documento;
+
+    public Persona(String nombres, String apellidos, String documento) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.documento = documento;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+    
+}
